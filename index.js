838ebee192f20a25ea545d2deb20837193f2be65
RichardVerbraak <RichardVerbraak@users.noreply.github.com>
2022-03-07 18:12:05 +0100

    Fixing another old project of mine written in VanillaJS -- now the current users IP address will be shown on the map upon initial page load and also on input search

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // DOM Elements
 const input = document.querySelector('.header__input--input')
 const button = document.querySelector('.header__input--btn')
-const map = document.querySelector('#mapid')
 
 // Panels
 const ipInfo = document.querySelector('.ip')
@@ -14,47 +13,65 @@ button.addEventListener('click', () => {
 	getIPAdress(input.value)
 })
 
-// const myMap = L.map('mapid').setView([51.505, -0.09], 13)
-// L.tileLayer(
-// 	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-// 	{
-// 		attribution:
-// 			'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-// 		maxZoom: 18,
-// 		id: 'mapbox/streets-v11',
-// 		tileSize: 512,
-// 		zoomOffset: -1,
-// 		accessToken:
-// 			'pk.eyJ1Ijoibm90eWV0cmF0ZWQiLCJhIjoiY2tlb2R6Mng4Mjc1eTJ5bnAyZDN4YXU3MSJ9.YXRNzz7lF9FGnSD6QPvB2Q',
-// 	}
-// ).addTo(myMap)
+// Initialize the map with own IP Address on page load
 
+const map = L.map('map', { zoomControl: false })
+
+L.tileLayer(
+	'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+	{
+		attribution:
+			'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		maxZoom: 18,
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1,
+		accessToken:
+			'pk.eyJ1Ijoibm90eWV0cmF0ZWQiLCJhIjoiY2tlb2R6Mng4Mjc1eTJ5bnAyZDN4YXU3MSJ9.YXRNzz7lF9FGnSD6QPvB2Q',
+	}
+).addTo(map)
+
+// Draws a new map + a marker based on the locations latitude and longitude
+const drawNewMap = (lat, lng) => {
+	// Changes the map to the new coordinates
+	map.setView([lat, lng], 13)
+
+	const myIcon = L.icon({
+		iconUrl: './images/icon-location.svg',
+		iconSize: [40, 50],
+		iconAnchor: [22, 94],
+		popupAnchor: [-3, -76],
+		shadowSize: [68, 95],
+		shadowAnchor: [22, 94],
+	})
+
+	// Adds the marker
+	L.marker([lat, lng], { icon: myIcon }).addTo(map)
+}
+
+// Gets the location based on the IP address from the input
 const getIPAdress = async (address) => {
-	const res = await fetch(`https://geo.ipify.org/api/v1?apiKey=at_6SceFSek4Lwg9pbTODk2tFenU1JX2&ipAddress=${address}
-    `)
+	// Fetch location based on IP
+	const res = await fetch(
+		`https://geo.ipify.org/api/v1?apiKey=at_6SceFSek4Lwg9pbTODk2tFenU1JX2&${
+			address && `ipAddress=${address}`
+		}`
+	)
+
 	const data = await res.json()
 
+	// Destructure needed data
 	const { ip, location, isp } = data
 	const { city, country, region, timezone, lat, lng } = location
 
+	// Change the DOM of the header to reflect the fetched data
 	ipInfo.textContent = `${ip}`
 	locationInfo.textContent = `${city}, ${country} ${region}`
 	timezoneInfo.textContent = `${timezone}`
 	ispInfo.textContent = `${isp}`
 
-	let myMap = L.map('mapid').setView([`${lat}`, `${lng}`], 13)
-	L.tileLayer(
-		'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
-		{
-			attribution:
-				'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-			maxZoom: 18,
-			id: 'mapbox/streets-v11',
-			tileSize: 512,
-			zoomOffset: -1,
-			accessToken:
-				'pk.eyJ1Ijoibm90eWV0cmF0ZWQiLCJhIjoiY2tlb2R6Mng4Mjc1eTJ5bnAyZDN4YXU3MSJ9.YXRNzz7lF9FGnSD6QPvB2Q',
-		}
-	).addTo(myMap)
-	console.log(data)
+	// Draw the new Leaflet map
+	drawNewMap(lat, lng)
 }
+
+getIPAdress()
